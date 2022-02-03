40f3dc29431885ef3fa6c5b45a7acb7267a6a321
rokeby <gary.zhexi.zhang@gmail.com>
2022-02-03 19:21:25 +0000

    first commit

---
+++ twttr-video-dl.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# script for downloading 
+
+# array of tweets with videos to be downloaded
+YOUTUBEDL_TWTTR_VIDEOS=('https://twitter.com/IwriteOK/status/1489058710831861763' 
+	'https://twitter.com/KwajoHousing/status/1488850915281022978' 
+	'https://twitter.com/IwriteOK/status/1489058710831861763')
+ 
+# iterate through array with youtube-dl
+for video in "${YOUTUBEDL_TWTTR_VIDEOS[@]}"
+do
+  echo "downloading video from $video, please wait ..."
+  youtube-dl "-o %(title).100s-%(id)s.%(ext)s" $video   # limits video title to 100 characters so youtube-dl doesn't break
+  echo "done"
+done
